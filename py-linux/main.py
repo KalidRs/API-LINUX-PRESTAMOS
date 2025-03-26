4027aab873a3e7384e6c349f82e39a94e84e6fc3
Brayn Kalid Reyes Silva <brayn4krs@gmail.com>
2025-03-25 22:00:56 -0600

    UPD: url a vercel

--- py-linux/main.py
+++ py-linux/main.py
@@ -1,32 +1,30 @@
 from fastapi import FastAPI, Depends
 from fastapi.security import HTTPBearer
-from routes.usersRoutes import user, auth_router  # Asegúrate de importar auth_router
-from routes.materialRoutes import material
-from routes.loanRoutes import loan
 from fastapi.middleware.cors import CORSMiddleware
 
-origins = [
-    "http://localhost:3000",
-]
+from routes.usersRoutes import user, auth_router
+from routes.materialRoutes import material
+from routes.loanRoutes import loan
 
 app = FastAPI(
-    title="API  ",
-    description="API de pruebas con rutas protegidas "
+    title="API",
+    description="API de pruebas con rutas protegidas"
 )
 
-# Configurar autenticación con HTTPBearer
-security = HTTPBearer()
-
-# Registrar rutas (agregar autenticación solo en rutas protegidas)
-app.include_router(user)
-app.include_router(auth_router)  
-app.include_router(material)
-app.include_router(loan)
-
+# Middleware CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://front-prestamos.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Autenticación con HTTPBearer
+security = HTTPBearer()
+
+# Incluir routers
+app.include_router(user)
+app.include_router(auth_router)
+app.include_router(material)
+app.include_router(loan)
